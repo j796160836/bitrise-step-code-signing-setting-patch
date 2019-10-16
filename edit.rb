499f4872d8eaa65d8358668983d1ad01f876f996
Johnny <>
2019-10-16 19:12:30 +0800

    implement code-signing-setting-patch

---
+++ edit.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/env ruby
+
+require 'xcodeproj'
+
+$xcode_xcodeproj_file = ENV['xcode_xcodeproj_file']
+$project_target = ENV['project_target']
+$code_sign_style = ENV['code_sign_style']
+$release_development_team = ENV['release_development_team']
+$release_code_sign_identity = ENV['release_code_sign_identity']
+$release_provisioning_profile_specifier = ENV['release_provisioning_profile_specifier']
+$debug_development_team = ENV['debug_development_team']
+$debug_code_sign_identity = ENV['debug_code_sign_identity']
+$debug_provisioning_profile_specifier = ENV['debug_provisioning_profile_specifier']
+
+project = Xcodeproj::Project.open($xcode_xcodeproj_file)
+
+$CONFIGURATION_NAMES = ["Debug", "Release"]
+
+def getTarget(project, name)
+	project.targets.each do |target|
+		if target.name == name
+			return target
+		end
+	end
+	return nil
+end
+
+def getBuildSettings(target, name)
+	target.build_configurations.each do |build_configurations|
+		if build_configurations.name == name
+			return build_configurations.build_settings
+		end
+	end
+	return nil
+end
+
+def getBuildSettingsStrings(project, target_name, configuration_name)
+
+	target = getTarget(project, target_name)
+	settings = getBuildSettings(target, configuration_name)
+
+	result = ""
+	result += "Build Configuration: " + configuration_name + "\n"
+	result += "CODE_SIGN_STYLE: " + settings["CODE_SIGN_STYLE"] + "\n"
+	result += "DEVELOPMENT_TEAM: " + settings["DEVELOPMENT_TEAM"] + "\n"
+	result += "CODE_SIGN_IDENTITY: " + settings["CODE_SIGN_IDENTITY"] + "\n"
+	result += "PROVISIONING_PROFILE_SPECIFIER: " + settings["PROVISIONING_PROFILE_SPECIFIER"]
+	return result
+end
+
+def printAllBuildSettings(project, target_name)
+
+	puts "-----------------------------"
+	puts "Target: " + target_name
+	$CONFIGURATION_NAMES.each do |configuration_name|
+		puts
+		puts getBuildSettingsStrings(project, target_name, configuration_name)
+	end
+	puts "-----------------------------"
+end
+
+puts "\# Original Settings:"
+
+printAllBuildSettings(project, $project_target)
+
+
+target = getTarget(project, $project_target)
+debugSettings = getBuildSettings(target, $CONFIGURATION_NAMES[0])
+debugSettings["CODE_SIGN_STYLE"] = $code_sign_style
+debugSettings["DEVELOPMENT_TEAM"] = $debug_development_team
+debugSettings["CODE_SIGN_IDENTITY"] = $debug_code_sign_identity
+debugSettings["PROVISIONING_PROFILE_SPECIFIER"] = $debug_provisioning_profile_specifier
+
+releaseSettings = getBuildSettings(target, $CONFIGURATION_NAMES[1])
+releaseSettings["CODE_SIGN_STYLE"] = $code_sign_style
+releaseSettings["DEVELOPMENT_TEAM"] = $release_development_team
+releaseSettings["CODE_SIGN_IDENTITY"] = $release_code_sign_identity
+releaseSettings["PROVISIONING_PROFILE_SPECIFIER"] = $release_provisioning_profile_specifier
+
+puts
+puts "\# Changed Settings:"
+
+printAllBuildSettings(project, $project_target)
+
+project.save
--- step.sh
+++ step.sh
@@ -5,12 +5,12 @@ set -e
 
 #
 # Required parameters
-if [ -z "${xcode_pbxproj_file}" ] ; then
-  echo " [!] Missing required input: xcode_pbxproj_file"
+if [ -z "${xcode_xcodeproj_file}" ] ; then
+  echo " [!] Missing required input: xcode_xcodeproj_file"
   exit 1
 fi
-if [ ! -f "${xcode_pbxproj_file}" ] ; then
-  echo " [!] File project.pbxproj doesn't exist at specified path: ${xcode_pbxproj_file}"
+if [ ! -f "${xcode_xcodeproj_file}/project.pbxproj" ] ; then
+  echo " [!] File project.pbxproj doesn't exist at specified path: ${xcode_xcodeproj_file}"
   exit 1
 fi
 
@@ -24,16 +24,6 @@ if [ -z "${code_sign_style}" ] ; then
   exit 1
 fi
 
-if [ -z "${release_code_sign_identity}" ] ; then
-  echo " [!] Missing required input: release_code_sign_identity"
-  exit 1
-fi
-
-if [ -z "${release_provisioning_profile_specifier}" ] ; then
-  echo " [!] Missing required input: release_provisioning_profile_specifier"
-  exit 1
-fi
-
 if [ -z "${debug_code_sign_identity}" ] ; then
   echo " [!] Missing required input: debug_code_sign_identity"
   exit 1
@@ -44,17 +34,32 @@ if [ -z "${debug_provisioning_profile_specifier}" ] ; then
   exit 1
 fi
 
+if [ -z "${release_code_sign_identity}" ] ; then
+  echo " [!] Missing required input: release_code_sign_identity"
+  exit 1
+fi
+
+if [ -z "${release_provisioning_profile_specifier}" ] ; then
+  echo " [!] Missing required input: release_provisioning_profile_specifier"
+  exit 1
+fi
+
 
 # ---------------------
 # --- Configs:
 
-echo " (i) Provided project.pbxproj Path                     : ${xcode_pbxproj_file}"
+echo " (i) Provided Project's .xcodeproj Path                : ${xcode_xcodeproj_file}"
 echo " (i) Provided Project Target                           : ${project_target}"
 echo " (i) Provided Code Sign Style                          : ${code_sign_style}"
-echo " (i) Provided Development Team (Release)               : ${release_development_team}"
-echo " (i) Provided Code Sign Identity (Release)             : ${release_code_sign_identity}"
-echo " (i) Provided Provisioning Profile Specifier (Release) : ${release_provisioning_profile_specifier}"
+echo
 echo " (i) Provided Development Team (Debug)                 : ${debug_development_team}"
 echo " (i) Provided Code Sign Identity (Debug)               : ${debug_code_sign_identity}"
 echo " (i) Provided Provisioning Profile Specifier (Debug)   : ${debug_provisioning_profile_specifier}"
+echo
+echo " (i) Provided Development Team (Release)               : ${release_development_team}"
+echo " (i) Provided Code Sign Identity (Release)             : ${release_code_sign_identity}"
+echo " (i) Provided Provisioning Profile Specifier (Release) : ${release_provisioning_profile_specifier}"
 
+
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ruby $THIS_SCRIPT_DIR/edit.rb
